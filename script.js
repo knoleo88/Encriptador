ca7efb269d0782495373fe15c6dc8858bfff5780
Leonardo Cano <leonardo_canito@hotmail.com>
2023-06-07 11:14:57 -0600

    Se realiza configuracion completa de logica de programaicon propia junto con la entrega que dieron los profesores

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 //Declaracion de constantes donde seleccionaremos los elementos de html que interactuaran con Javascript
 const textArea = document.querySelector(".text-area");//Aqui asignamos el primer elemento de la calse texarea de html a la vaiablre declarada
 const message = document.querySelector(".textarea2");//Aqui asignamos el primer elemento de la calse message de html a la vaiablre declarada
+const messageimg = document.querySelector(".menssageimg");
 const copy = document.querySelector(".Copy");
 copy.style.display = "none"// Ocultamos el elemento copy que hace referencia al botond e copyr en la interfaz grafica
+message.style.display = "none"
 
 
 function validateText(){ //Funcion que se encargara de validar el texto cumpla con las normas de ser solo minusculas y sin acentos
     let textwrten= document.querySelector(".text-area").value; // Se declara variabale que tendra el valor(cadena) del text area 
-    let validator = textwrten.match(/^[a-z]*$/); // Se crea variable para indicar el texto valido para el encriptador
+    let validator = textwrten.match(/^[a-z\s]*$/); // Se crea variable para indicar el texto valido para el encriptador
     //Es muy importante esta parte ya que se esta usando expresiones regualres donde ^[a-z]*$ indica que solo aceptaran valores de la a-z minusculas
     //y el ultimo * es para inidcar que es toda la linea y que los espacios antes y despues del ultimo valor sean contados tambien 
 
@@ -24,10 +26,11 @@ function btnEncrypt(){//Funcion de encrypt llamada desde el boton del mismo nomb
     if(!validateText()) {// Operacion logica que manda a llamar la funcion validar texto para ver que sean caracteres validos
         const encryptedtext = Encrypt(textArea.value) //en caso de ser true se da de alta nnueva variable para texto encriptado que manda a llamar funcion Encrypt y nos regresa el valor encriptado
         message.value = encryptedtext //Se coloca evalor de la variable texto encriptado en el text area que corresponden al resultado de Encrypt la informacion
-        message.style.backgroundImage = "none"//Se oculta imagen del muñeco
+        messageimg.style.display = "none"//Se oculta imagen del muñeco
         textArea.value = "";//Se borra el valor del text area donde se ingreso el primer texto
+        message.style.display = "block"//Se muestra textarea del mensaje encriptado
         copy.style.display = "block" //Se muestra boton copyr en el menu para que el usuario lo visualice
-    
+        message.disabled = true;//Se desabilita l aopcion de editar el textarea
     }
 }
 
@@ -42,13 +45,15 @@ function btnEncrypt(){//Funcion de encrypt llamada desde el boton del mismo nomb
 function Encrypt(stringencrypt){
     let matrizCode = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]]; // Aqui se declara un array multidimencional para este caso de 2x5
     stringencrypt = stringencrypt.toLowerCase() //Esta linea nos convertira todas las letras ingresadas en la variable string encriptada en minusculas
-
-    for(let i = 0; i < matrizCode.length; i++){//For para recorrer toda la matriz (matrizCode.length= tamaño de la matriz)
+    if (stringencrypt==""){ // Condicion que se ocupa para validar el text are ano este vacio y se recarga pagina en caso de que asi sea.
+        alert("Sin texto en el encriptador favor de ingresar texto para encriptar.")
+        location.reload();
+    }else {for(let i = 0; i < matrizCode.length; i++){//For para recorrer toda la matriz (matrizCode.length= tamaño de la matriz)
         if(stringencrypt.includes(matrizCode[i][0])){// Concicion indica que si en la matriz incluye la posision 0,0(seria una e) nos devuelve true y realiza la accion 
             stringencrypt = stringencrypt.replaceAll(matrizCode[i][0], matrizCode[i][1])//Donde aqui nos indica que se remplace la posicion de ejemplo inicial 0,0 por 0,1
 
         }
-
+    }
     }
     return stringencrypt // Se regresa el valor de varibale utilizada.
 }
@@ -59,19 +64,31 @@ function btnDesEncrypt(){ // Funcion para decrypt
     const encryptedtext = decrypt(textArea.value)//Se crea variable con el resultado de la funcion decrypt donde se manda llamar esta funcion
     message.value = encryptedtext // se coloca el resultado de la desencriptaicon en el textarea del message
     textArea.value = ""; //Se borra la informacion del text area
-    
+    messageimg.style.display = "none"//Se oculta imagen del muñeco
+    textArea.value = "";//Se borra el valor del text area donde se ingreso el primer texto
+    message.style.display = "block"//Se muestra textarea del mensaje encriptado
+    copy.style.display = "block" //Se muestra boton copyr en el menu para que el usuario lo visualice
+    message.disabled = true;//Se desabilita l aopcion de editar el textarea
 }
 
 
 function decrypt(stringdecrypt){ // Funcion decrypt
-    let matrizCode = [["i", "imes"],["e", "enter"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]]; // Aqui se declara un array multidimencional para este caso de 2x5
-
+    let validatedecrypt=stringdecrypt;//Se guarda el valor de la cadena para ver si hay diferencia al descoprimir
+    let matrizCode = [["a", "ai"],["i", "imes"],["e", "enter"],["o", "ober"], ["u", "ufat"]]; // Aqui se declara un array multidimencional para este caso de 2x5
+    if (stringdecrypt==""){ // Condicion que se ocupa para validar el text are ano este vacio y se recarga pagina en caso de que asi sea.
+        alert("Sin texto en el encriptador favor de ingresar texto para encriptar.")
+        location.reload();
+    } else {
     for(let i = 0; i < matrizCode.length; i++){// condicion para recorrer el array
         if(stringdecrypt.includes(matrizCode[i][1])){ // Concicion indica que si en la matriz incluye la posision 0,1(seria un enter) nos devuelve true y realiza la accion 
             stringdecrypt = stringdecrypt.replaceAll(matrizCode[i][1] , matrizCode[i][0])//Donde aqui nos indica que se remplace la posicion de ejemplo inicial 0,1 por 0,0
 
         }
-
+    }
+    if(validatedecrypt==stringdecrypt){
+        alert("El texto ingresado no esta encriptado, favor de intentarlo nuevamente")
+        location.reload();
+    }
     }
     return stringdecrypt//Se retona el valor de la variable utilizada
 }
